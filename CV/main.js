30c4f85a2e0646e5e5031746e465102ca01602f8
Patrick Cowhill <patwilcow@yahoo.com>
2024-07-27 13:14:39 -0400

    Adding back CV page

---
+++ CV/main.js
@@ -0,0 +1,84 @@
+// ============================================
+// Import, Initialization, and Preparation
+// ============================================
+import {
+  CATEGORY,
+  SECTION,
+  SUBSECTION,
+  ENTRY,
+  SUBENTRY,
+  textContents
+} from "./textContents.js";
+let dynamicContent = document.getElementById("dynamicContent");
+const ignoreFields = ["type", "description"];
+
+// ============================================
+// Compose the page HTML contents
+// ============================================
+dynamicContent.innerHTML = generateHTMLfromDictionary(textContents);
+
+// ============================================
+// Support Functions
+// ============================================
+function generateHTMLfromDictionary(dict) {
+  let returnHTML = "";
+  const keys = Object.keys(dict);
+  if (keys.includes("description")) {
+    returnHTML += tagwrap("i", dict["description"]);
+  }
+  switch (dict.type) {
+    case CATEGORY:
+      for (let category of keys) {
+        if (ignoreFields.includes(category)) continue;
+        returnHTML += tagwrap("h1", category);
+        returnHTML += "<hr>";
+        returnHTML += generateHTMLfromDictionary(dict[category]);
+      }
+      break;
+    case SECTION:
+      for (let section of keys) {
+        if (ignoreFields.includes(section)) continue;
+        returnHTML += tagwrap("h2", section);
+        returnHTML += generateHTMLfromDictionary(dict[section]);
+      }
+      break;
+    case SUBSECTION:
+      for (let subsection of keys) {
+        if (ignoreFields.includes(subsection)) continue;
+        returnHTML += '<div class="boxed">'
+        returnHTML += tagwrap("h3", subsection);
+        returnHTML += generateHTMLfromDictionary(dict[subsection]);
+        returnHTML += '</div>'
+      }
+      break;
+    case ENTRY:
+      for (let subsection of keys) {
+        if (ignoreFields.includes(subsection)) continue;
+        returnHTML += tagwrap("h4", subsection);
+        returnHTML += generateHTMLfromDictionary(dict[subsection]);
+      }
+      break;
+    case SUBENTRY:
+      //returnHTML += '<div class="entry">';
+      if (keys.includes("text")) {
+        //returnHTML += dict["text"];
+        returnHTML += tagwrap("p", dict["text"]);
+      }
+      if (keys.includes("list")) {
+        returnHTML += "<ul>";
+        for (let item of dict["list"]) {
+          returnHTML += tagwrap("li", '<span class="liEntry">' + item + "</span>");
+        }
+        returnHTML += "</ul>";
+      }
+      //returnHTML += "</div>";
+      break;
+    default:
+      return "ERROR: INVALID DICTIONARY TYPE."
+  }
+  return returnHTML;
+}
+
+function tagwrap(tag, contents) {
+  return "<" + tag + ">" + contents + "</" + tag + ">";
+}
