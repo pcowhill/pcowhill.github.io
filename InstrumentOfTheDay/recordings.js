1163cb2e57cdc16fb377b47880da19c2c6bb73d0
Patrick Cowhill <patwilcow@yahoo.com>
2024-07-27 15:53:05 -0400

    Technically, everything is finished!  Just gotta add more instruments and more f-l-a-v-o-r.

---
+++ InstrumentOfTheDay/recordings.js
@@ -0,0 +1,14 @@
+export const recordings = [
+    [
+        "ACCORDIAN",
+        "https://s9.imslp.org/files/imglnks/usimg/4/42/IMSLP310530-PMLP501767-Oddysey-Suite-192.mp3"
+    ],
+    [
+        "FLUTE",
+        "https://ks15.imslp.org/files/imglnks/usimg/0/05/IMSLP131520-PMLP246671-ConcertoForInstrumentsSolo(DWScott).ogg"
+    ],
+    [
+        "PIANO",
+        "https://s9.imslp.org/files/imglnks/usimg/d/d4/IMSLP671492-PMLP1077467-Album_Leaf_No.3-Lazy_Clouds_in_the_Afternoon-sv-.mp3"
+    ]
+];
--- InstrumentOfTheDay/sketch.js
+++ InstrumentOfTheDay/sketch.js
@@ -1,3 +1,5 @@
+import { recordings } from "./recordings.js";
+
 let guesses = [
   document.getElementById("firstGuess"),
   document.getElementById("secondGuess"),
@@ -7,6 +9,8 @@ let guesses = [
   document.getElementById("sixthGuess")
 ];
 
+let audioSelection = document.getElementById("audioSelection");
+
 let inputGuess = document.getElementById("inputGuess");
 let guessButton = document.getElementById("guessButton");
 let instrumentsList = document.getElementById("instrumentsList");
@@ -15,6 +19,29 @@ let feedback = document.getElementById("feedback");
 let correctAnswer = "accordion";
 let guessNumber = 0;
 let hasFinished = false;
+
+
+function setup() {
+  let randomSelection = Math.floor(Math.random() * recordings.length);
+  correctAnswer = recordings[randomSelection][0];
+  let correctRecording = recordings[randomSelection][1];
+  audioSelection.innerHTML = '<audio controls><source src="' + correctRecording + '"></audio>';
+  let validInstrumentsList = [];
+  for (let i = 0; i < recordings.length; i++) {
+    if (!validInstrumentsList.includes(recordings[i][0])) {
+      validInstrumentsList.push(recordings[i][0].toUpperCase());
+    }
+  }
+  validInstrumentsList.sort();
+  instrumentsList.innerHTML = "";
+  for (let i = 0; i < validInstrumentsList.length; i++) {
+    instrumentsList.innerHTML = instrumentsList.innerHTML + '<option value="' + validInstrumentsList[i] + '"></option>';
+  }
+}
+
+
+setup();
+
 
 function processGuess() {
   let inputValue = inputGuess.value;
@@ -32,6 +59,7 @@ function processGuess() {
     guesses[guessNumber].innerHTML = "<mark>" + guesses[guessNumber].innerText + "</mark>";
   }
   else {
+    feedback.innerText = inputValue.toUpperCase() + " is incorrect... try again...";
     if (guessNumber == 5) {
       feedback.innerText = "You LOOOOOOOOOSE!!!";
       hasFinished = true;
